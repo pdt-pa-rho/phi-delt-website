952634da32379686a916e88d4dcaa18ac1b570df
Rohan Jain <rjain@imsa.edu>
2025-03-30 16:12:11 -0400

    ls

--- src/app/rush/page.tsx
+++ src/app/rush/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
 
 export default function RushPage() {
   // Sample rush events - update with actual dates and events
