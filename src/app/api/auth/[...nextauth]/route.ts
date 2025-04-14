88de9e3eacc3de0a1c5d8ad3b9a1dfddf5bf4136
Rohan Jain <rjain@imsa.edu>
2025-04-14 16:44:41 -0400

    fire

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,7 +8,7 @@ const ALLOWED_EMAILS = [
   "allowed2@example.com",
 ];
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
